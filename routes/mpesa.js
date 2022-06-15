937cc3343ee7fd7efc708552cddedf5db52135cf
NicksyRop <nicksonkipkorir25@gmail.com>
2022-06-15 15:44:29 +0300

    changed

--- routes/mpesa.js
+++ routes/mpesa.js
@@ -2,19 +2,31 @@ const router = require("express").Router();
 
 let unirest = require("unirest");
 router.get("/authorization", (req, res) => {
-  let req = unirest(
-    "GET",
-    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+  var myHeaders = new Headers();
+  myHeaders.append(
+    "Authorization",
+    "Basic QXpzMktlalUxQVJ2SUw1SmRKc0FSYlYyZ0RyV21wT0I6aGlwR3ZGSmJPeHJpMzMwYw=="
+  );
+  myHeaders.append(
+    "Cookie",
+    "incap_ses_1102_2742146=YJw0VTawYXs23fj/yRdLD6nUqWIAAAAAgCz8GazqEsqNjZNRVz0/aQ==; visid_incap_2742146=DO94YXlSQry6A5caofrTh6nUqWIAAAAAQUIPAAAAAAB+2R7ksifuretC3CTaMBMQ"
+  );
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(
+    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+    requestOptions
   )
-    .headers({
-      Authorization:
-        "Bearer cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ==",
+    .then((response) => response.json())
+    .then((result) => {
+      return res.send(result);
     })
-    .send()
-    .end((response) => {
-      if (response.error) throw new Error(response.error);
-      return res.send(response);
-    });
+    .catch((error) => console.log("error", error));
 });
 
 module.exports = router;
